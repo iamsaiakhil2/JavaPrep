3db82cfa52f8fdce1aa6d37c414675e23fd23330
iamsaiakhil2 <iamsaiakhil@outlook.com>
2025-02-14 13:29:13 -0800

    Started practicing OOPS

--- src/OopsDemo.java
+++ src/OopsDemo.java
@@ -17,3 +17,21 @@ class Laptop {
         System.out.println("Running Smooth Laptop");
     }
 }
+class Student {
+    int id;
+    void info() {
+        id = 4;
+        double avg= 4.5;
+        System.out.println(id + "   "+ avg);
+    }
+// Instance variables are such variables which are created or declared directly inside class outside any block or method
+// instance variable memory will be allocated on heap area inside the object or instance
+// Default value will be initialized for instance variable
+//    Memory will be deallocated by garbage collector when object becomes reference less
+//    ------------------
+//    local variables are such variables which are declared inside a method or block of a class
+//    Default values are not initialized
+//    Memory will be inside stack frame of the method or block within stack area
+//    Once method execution is over the memory for local variable will be deallocated
+
+}
